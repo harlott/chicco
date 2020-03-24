0d68cd7c403277bf9c43942dd5f15e58a3fabede
marco.allotta <marco.allotta@chili.com>
2020-03-24 11:50:59 +0100

    favicon - completed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,28 +17,105 @@ const TemplateWrapper = ({ children }) => {
 
         <link
           rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
+          sizes="57x57"
+          href={`${withPrefix('/')}img/favicon/apple-icon-57x57.png`}
         />
 
         <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href={`${withPrefix('/')}img/favicon/apple-icon-60x60.png`}
         />
-        <meta name="theme-color" content="#fff" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href={`${withPrefix('/')}img/favicon/apple-icon-72x72.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href={`${withPrefix('/')}img/favicon/apple-icon-76x76.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href={`${withPrefix('/')}img/favicon/apple-icon-114x114.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href={`${withPrefix('/')}img/favicon/apple-icon-120x120.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href={`${withPrefix('/')}img/favicon/apple-icon-144x144.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href={`${withPrefix('/')}img/favicon/apple-icon-152x152.png`}
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${withPrefix('/')}img/favicon/apple-icon-180x180.png`}
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={`${withPrefix('/')}img/favicon/android-icon-192x192.png`}
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${withPrefix('/')}img/favicon/favicon-32x32.png`}
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href={`${withPrefix('/')}img/favicon/favicon-96x96.png`}
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${withPrefix('/')}img/favicon/favicon-16x16.png`}
+        />
+
+        <link
+          rel="manifest"
+          href={`${withPrefix('/')}img/favicon/manifest.json`}
+        />
+
+        <meta
+          name="msapplication-TileColor"
+          content="#ffffff"
+        />
+
+        <meta
+          name="msapplication-TileImage"
+          content={`${withPrefix('/')}img/favicon/ms-icon-144x144.png`}
+        />
+      
+        <meta
+          name="theme-color"
+          content="#ffffff"
+        />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
