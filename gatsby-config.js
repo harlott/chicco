b2edc76e944986b338c4d0952e53fcfcfce591d8
marco.allotta <marco.allotta@chili.com>
2020-03-24 12:17:30 +0100

    metadata - completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Chicco Allotta - Pianist, arranger, composer: based in London!',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Chicco Allotta: A soulful pianist in a supernatural world.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
