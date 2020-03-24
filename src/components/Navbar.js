8c46c447101f1d7be16b64018cbfa84e45e893c5
marco.allotta <marco.allotta@chili.com>
2020-03-24 13:10:18 +0100

    navbar new items + new images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,29 +64,17 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Video
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Photo
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                Tour
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
