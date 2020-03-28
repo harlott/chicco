43226b7d943b11cbc1ae6d753e57618f526b9c16
marco.allotta <marco.allotta@chili.com>
2020-03-28 08:38:04 +0100

    projects collection added

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,6 +6,7 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import GalleryPagePreview from './preview-templates/GalleryPagePreview'
 import VideoPagePreview from './preview-templates/VideoPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ProjectsPostPreview from './preview-templates/ProjectsPagePreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
@@ -16,5 +17,6 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('projects', ProjectsPostPreview)
 CMS.registerPreviewTemplate('gallery', GalleryPagePreview)
 CMS.registerPreviewTemplate('video', VideoPagePreview)
