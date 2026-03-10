86c170734b4890e3e46c43d73f9314c4a3b80ae2
Marco Allotta <marcoallott@gmail.com>
2026-03-10 17:54:31 +0100

    Integrate HomePage component into CMS preview

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,21 @@
 import dynamic from "next/dynamic";
 import { useEffect } from 'react';
 import DECAP_CMS_CONFIG from "../app/lib/DecapCmsConfig";
-
+import HomePage from '../ui/HomePage.jsx';
 const CMS = dynamic(
   () => {
       return import("decap-cms-app")
          .then((mod) =>  {
             mod.default.init({ config: DECAP_CMS_CONFIG });
+           CMS.registerPreviewStyle("/styles/foundation.css");
+           CMS.registerPreviewStyle("/styles/style.css");
+           CMS.registerPreviewStyle("/styles/patch.css");
+           CMS.registerPreviewStyle("/styles/fontello.css");
+
+           CMS.registerPreviewTemplate("pages", ({ entry, widgetFor }) => {
+             const data = entry.get("data").toJS()
+             return <HomePage data={data} body={widgetFor("body")} />
+           });
          });
    },{ loading: () => null, ssr: false }
 );
