9b2dc5c1809a01875f6a157e87f01fbbc395aa39
Marco Allotta <marcoallott@gmail.com>
2026-03-30 12:12:51 +0200

    Increase icon size for social media links

--- app/ui/SocialSection.jsx
+++ app/ui/SocialSection.jsx
@@ -6,7 +6,7 @@ function SocialSection() {
     <div className="row" style={{ marginTop: '4%' }}>
       <div className="large-8 columns large-centered">
         <div className="section-title">
-          <Link href="/"><span className="fa-brands fa-facebook" style={{ color: '#1877f2' }} /></Link> | <Link href="/"><span className="fa-brands fa-instagram" style={{ color: '#e4405f' }} /></Link> | <Link href="/"><span className="fa-brands fa-youtube" style={{ color: '#ff0000' }} /></Link>
+          <Link href="/"><span className="fa-brands fa-facebook" style={{ color: '#1877f2', fontSize: '48px' }} /></Link> | <Link href="/"><span className="fa-brands fa-instagram" style={{ color: '#e4405f', fontSize: '48px' }} /></Link> | <Link href="/"><span className="fa-brands fa-youtube" style={{ color: '#ff0000', fontSize: '48px' }} /></Link>
         </div>
       </div>
     </div>
