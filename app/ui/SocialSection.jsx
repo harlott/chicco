d828e17a1c821bcef12ef20a01a3efc76dad7208
Marco Allotta <marcoallott@gmail.com>
2026-03-30 11:35:06 +0200

    Add SocialSection component for social links

---
+++ app/ui/SocialSection.jsx
@@ -0,0 +1,13 @@
+function SocialSection() {
+  return(
+    <div className="row" style={{ marginTop: '4%' }}>
+      <div className="large-8 columns large-centered">
+        <div className="section-title">
+          <a href="/">Facebook</a> | <a href="/">Instagram</a> | <a href="/">Youtube</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialSection;
